cf1614d8e6956b1f18da3860915d0bb8ffeb4cd1
Lisa-Maria Plakolb <lisa.plakolb96@gmx.at>
2020-02-12 14:21:05 +0100

    Added Solution

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a few numbers:");
+        Scanner scan = new Scanner(System.in);
+
+        int[] numbers = new int[0];
+
+        while (!scan.hasNext("q") || scan.hasNext("Q")) {
+            int newValue = scan.nextInt();
+            int[] newArray = new int[numbers.length + 1];
+            for (int i = 0; i < numbers.length; i++) {
+                newArray[i] = numbers[i];
+            }
+            newArray[numbers.length] = newValue;
+            numbers = newArray;
+        }
+
+        //calculate maximum
+        int checkMax = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (checkMax < numbers[i]) {
+                checkMax = numbers[i];
+            }
+
+        }
+
+        System.out.println(checkMax);
+
+    }
+}
